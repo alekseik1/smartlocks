b49faf9688a9be6d0aff054052957108e02bdbd5
alekseik1 <1alekseik1@gmail.com>
2021-04-27 23:37:42 +0300

    feat(logger): pass log level

--- log_writing.py
+++ log_writing.py
@@ -4,5 +4,6 @@ from loguru import logger
 logger.add('logs/log_{time}.log', rotation=datetime.timedelta(days=1))
 
 
-def print_log(s):
-    logger.info(s)
+def print_log(s, level: str = 'INFO'):
+    level = level.upper()
+    logger.log(level, s)
