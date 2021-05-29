d6651719b07879bde9217b536f1eebe5fda8f05d
alekseik1 <1alekseik1@gmail.com>
2021-05-29 22:15:04 +0300

    feat(bot): close door after 3 seconds

--- admin_monitoring.py
+++ admin_monitoring.py
@@ -1,7 +1,8 @@
 import os
+import asyncio
 from device_manager import manager
 from loguru import logger
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, BackgroundTasks
 from dotenv import load_dotenv
 
 app = FastAPI()
@@ -10,12 +11,18 @@ load_dotenv()
 SECRET_TOKEN = os.environ.get('SECRET_BOT_TOKEN')
 
 
+async def close_door_after_delay(delay_seconds: int):
+    await asyncio.sleep(delay_seconds)
+    manager.door_magnet.close()
+
+
 @app.get('/door/open')
-async def open_admin_request(secret_token: str):
+async def open_admin_request(secret_token: str, background_tasks: BackgroundTasks):
     if secret_token != SECRET_TOKEN:
         logger.info('received incorrect token')
         return HTTPException(status_code=403, detail="Incorrect token")
     manager.door_magnet.open()
+    background_tasks.add_task(close_door_after_delay, delay_seconds=3)
     return 'ok'
 
 
